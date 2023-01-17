9ec217527c4638c638c73e6c8e7d15c0fa1fc796
Nimish Tolasaria <ntolasaria@icloud.com>
2023-01-17 20:55:39 +0530

    cli application - expenses

---
+++ lesson_1/expense/schema.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS expenses;
+
+CREATE TABLE expenses (
+  id serial PRIMARY KEY,
+  amount numeric(6,2) NOT NULL,
+  memo text NOT NULL,
+  created_on date NOT NULL
+);
+
+ALTER TABLE expenses
+  ADD CONSTRAINT positive_amount CHECK (amount >= 0.01);
+
+INSERT INTO expenses (amount, memo, created_on) VALUES (14.56, 'Pencils', NOW());
+INSERT INTO expenses (amount, memo, created_on) VALUES (3.29, 'Coffee', NOW());
+INSERT INTO expenses (amount, memo, created_on) VALUES (49.99, 'Text Editor', NOW());
